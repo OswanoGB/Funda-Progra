591524a098db92110b44a8cf2c12d3fb3c5120d5
Honeycomb666 <00222320@uca.edu.sv>
2020-06-13 22:32:04 -0600

    Update ejercicio3.cpp

--- CORTO3/ejercicio3.cpp
+++ CORTO3/ejercicio3.cpp
@@ -9,18 +9,18 @@ int main()
     int mod2;
     int mod3;
 
-    reply = 'y';
+    reply = 'y'; 
 
     while (reply == 'y' || reply == 'Y')
     {
         cout << "Ingrese el a\244o que desea comprobar:\n";
         cin >> year;
-        mod = year % 400;
+        mod = year % 400; // Operaciones para verificar si el año es divisible por los valores
         mod2 = year % 4;
         mod3 = year % 100;
-        if (mod == 0)
+        if (mod == 0) // IFs anidados para la comprobación e impresión del mensaje final
         {
-            cout << "El a\244o " << year << " es bisiesto.\n";
+            cout << "El a\244o " << year << " es bisiesto.\n"; 
         } else if (mod2 == 0 && mod3 != 0)
         {
             cout << "El a\244o " << year << " es bisiesto.\n";
@@ -29,7 +29,7 @@ int main()
             cout << "El a\244o " << year << " NO es bisiesto.\n";
         }
 
-        cout << "Desea comprobar otro a\244o? (y/n)\n";
+        cout << "Desea comprobar otro a\244o? (y/n)\n"; // Comprobación de la variable iniciadora del ciclo
         cin >> reply;
 
     }  
